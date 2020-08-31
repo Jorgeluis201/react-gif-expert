bb3a80c7deba85cb8115d025b9bbd6d4c4864a45
Jorge Barrera <j.barrerad@alumnos.duoc.cl>
2020-08-31 16:34:19 -0400

    second commi

---
+++ docs/precache-manifest.15473028a8d1a70b95af5c2c00ac3c16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c2a05aebd68d6fb10c4b92576ec908",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe07dec8e6277661e3b3",
+    "url": "/static/css/main.dea14762.chunk.css"
+  },
+  {
+    "revision": "4eb9ff5b03ad672ef2d0",
+    "url": "/static/js/2.745541f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.745541f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe07dec8e6277661e3b3",
+    "url": "/static/js/main.459e7cbe.chunk.js"
+  },
+  {
+    "revision": "7c7e71c3d13998346ba7",
+    "url": "/static/js/runtime-main.8f02d32f.js"
+  }
+]);
